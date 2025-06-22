b86a93db7a65e1aea183b68a5b5a9fcab434fd62
Edmond Amofah Boakye <edmondboakye1622@gmail.com>
2025-06-22 13:18:33 +0000

    fix footer gap

--- Client/src/layout/MainLayout.tsx
+++ Client/src/layout/MainLayout.tsx
@@ -12,9 +12,9 @@ const MainLayout: React.FC = () => {
 
   const isGamePlay = location.pathname.includes("/gameplay/");
   return (
-    <div className="min-h-screen bg-gray-900 text-white dark:bg-gray-100 dark:text-gray-900 transition-colors duration-300">
+    <div className="min-h-screen bg-gray-900 text-white dark:bg-gray-100 dark:text-gray-900 transition-colors duration-300 flex flex-col">
       <Navbar />
-      <main className=" bg-white dark:bg-[#0f1221]">
+      <main className="flex-1 bg-white dark:bg-[#0f1221]">
         <Outlet />
       </main>
       {!isGamePlay && (
